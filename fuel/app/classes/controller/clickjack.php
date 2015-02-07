69bb896109edb40ec890a2d82cd93fdb079b0b1c
Truong Dinh Tam <truong.tam@mulodo.com>
2015-02-07 16:20:04 +0900

    Click jack Countermeasure

---
+++ fuel/app/classes/controller/clickjack.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * クリックジャック
+ * 
+ * クリックジャック対策のための X-FRAME-OPTIONS ヘッダの値は
+ * DENY または SAMEORIGIN が 指定できますが、ここでは SAMEORIGIN にしておきます。
+ * DENY はそのページがフレーム内に表示 されることをすべて禁止します。
+ * SAMEORIGIN は同一ドメインのページのみでフレーム内に表示され ることを許可します。
+ */
+
+class Controller_Clickjack extends Controller_Template
+{
+
+    public function before()
+    {
+        echo 'tamtest';
+        parent::before();
+        $this->response = Response::forge();
+        $this->response->set_header('X-FRAME-OPTIONS', 'SAMEORIGIN');
+    }
+
+    public function after($response)
+    {
+        $response = $this->response;
+        $response->body = $this->template;
+        return parent::after($response);
+    }
+
+}
