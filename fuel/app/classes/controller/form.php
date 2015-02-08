1c45573955eee74231b1eae574b47a3c74c78ffe
Truong Dinh Tam <truong.tam@mulodo.com>
2015-02-08 18:26:20 +0900

    Use Fieldset for refactoring code

--- fuel/app/classes/controller/form.php
+++ fuel/app/classes/controller/form.php
@@ -5,22 +5,30 @@ class Controller_Form extends Controller_Template
 
     public function action_index()
     {
+        $form = $this->forge_form();
+        if(Input::method() === 'POST'){
+            $form->repopulate();
+        }
         $this->template->title = '問い合わせ';
         $this->template->content = View::forge('form/index');
+        $this->template->content->set_safe('html_form', $form->build('form/confirm'));
     }
 
     public function action_confirm()
     {
-        $val = $this->forge_validate();
-       
+        $form = $this->forge_form();
+        $val = $form->validation()->add_callable('MyValidationRules');
+
         if ($val->run()) {
             $data['input'] = $val->validated();
             $this->template->title = 'お問い合わせ: 確認';
             $this->template->content = View::forge('form/confirm', $data);
         } else {
+            $form->repopulate();
             $this->template->title = 'お問い合わせ: エラー';
             $this->template->content = View::forge('form/index');
             $this->template->content->set_safe('html_error', $val->show_errors());
+            $this->template->content->set_safe('html_form', $form->build('form/confirm'));
         }
     }
 
@@ -31,13 +39,15 @@ class Controller_Form extends Controller_Template
             throw new HttpInvalidInputException('ページ遷移が正しくありません');
         }
 
-        $val = $this->forge_validate();
-    
+        $form = $this->forge_form();
+        $val = $form->validation()->add_callable('MyValidationRules');
+        
         if (!$val->run()) {
             $this->template->title = 'お問い合わせ: エラー';
             $this->template->content = View::forge('form/index');
             $this->template->content->set_safe('html_error', $val->show_errors());
-
+            $this->template->content->set_safe('html_form', $form->build('form/confirm'));
+        
             return;
         }
 
@@ -58,35 +68,36 @@ class Controller_Form extends Controller_Template
             $html_error = '<p>メールを送信できませんでした。</p>';
         }
 
+        $form->repopulate();
         $this->template->title = 'お問い合わせ: 送信エラー';
         $this->template->content = View::forge('form/index');
         $this->template->content->set_safe('html_error', $html_error);
+        $this->template->content->set_safe('html_form', $form->build('form/confirm'));        
     }
 
-    // データ検証	
-    public function forge_validate()
+    // フォームの定義
+    public function forge_form()
     {
-        $val = Validation::forge();
-        $val->add_callable('MyValidationRules');
-
-        $val->add('name', '名前')
+        $form = Fieldset::forge();
+        $form->add('name', '名前')
                 ->add_rule('trim')
                 ->add_rule('required')
                 ->add_rule('no_tab_and_newline')
                 ->add_rule('max_length', 50);
         
-        $val->add('email', 'メールアドレス')
+        $form->add('email', 'メールアドレス')
                 ->add_rule('trim')
                 ->add_rule('required')
                 ->add_rule('no_tab_and_newline')
                 ->add_rule('max_length', 100)
                 ->add_rule('valid_email');
 
-        $val->add('comment', 'コメント')
+        $form->add('comment', 'コメント', array('type' => 'textarea', 'cols' => 70, 'rows' => 6))
                 ->add_rule('required')
                 ->add_rule('max_length', 400);
 
-        return $val;
+        $form->add('submit', '', array('type' => 'submit', 'value' => '確認'));
+        return $form;
     }
 
     // メールの作成
