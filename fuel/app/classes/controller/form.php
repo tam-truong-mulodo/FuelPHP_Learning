fa07d82296ca7f1f463f48ed2fbf7a2289d87832
Truong Dinh Tam <truong.tam@mulodo.com>
2015-02-16 23:49:38 +0900

    Use Model_Crud to connect and save data to mysql (only one table)

--- fuel/app/classes/controller/form.php
+++ fuel/app/classes/controller/form.php
@@ -51,6 +51,26 @@ class Controller_Form extends Controller_Clickjack
         }
 
         $post = $val->validated();
+        
+        //Ip, user agentの情報をpost データに追加
+        $post['ip_address'] = Input::ip();
+        $post['user_agent'] = Input::user_agent();
+        unset($post['submit']); //postデータからsubmitを削除
+        //DBへ保存する
+        $model_form = Model_Form::forge()->set($post);
+        list($id, $rows) = $model_form->save();
+        
+        if($rows != 1){
+            Log::error('データベース保存エラー', __METHOD__);
+            $form->repopulate();
+            $this->template->title = '問い合わせ: サーバエラー';
+            $this->template->content = View::forge('form/index');
+            $html_error = '<p>サーバでエラーが発生しました。</p>';
+            $this->template->content->set_safe('html_error', $html_error);
+            $this->template->content->set_safe('html_form', $form->build('form/confirm'));
+            return;
+        }
+
         // メールの送信
         try {
             $mail = new Model_Mail();
---
+++ fuel/app/classes/model/form.php
@@ -0,0 +1,9 @@
+<?php
+
+class Model_Form extends Model_Crud
+{
+
+    // 使用するテーブル名
+    protected static $_table_name = 'tbl_cf_form';
+
+}
